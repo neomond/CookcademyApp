28877104ab00e8ee38c416b3fafeb0883dcca306
neomond <neoweise@yahoo.com>
2024-09-08 00:25:35 +0400

    initial -feat: created recipes model

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Nazrin Atayeva on 07.09.24.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
